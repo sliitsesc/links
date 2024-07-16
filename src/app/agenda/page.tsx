f228839e7be9ef45abd70fdc7b2446ec12eeb990
dinal-qlub <pasindu.dinal@qlub.io>
2024-07-17 01:25:59 +0530

    chore: updated content

---
+++ src/app/agenda/page.tsx
@@ -0,0 +1,87 @@
+import EventCard from "@/components/EventCard";
+import HeaderAnimation from "@/components/HeaderAnimation/HeaderAnimation";
+import { AgendaData } from "@/data/AgendaData";
+import { CalendarFold, MapPin } from "lucide-react";
+import { Chakra_Petch, Plus_Jakarta_Sans } from "next/font/google";
+import Image from "next/image";
+import Link from "next/link";
+
+const plusJakartaSans = Plus_Jakarta_Sans({
+  weight: ["400"],
+  subsets: ["latin"],
+});
+
+const chakra = Chakra_Petch({
+  weight: ["400", "600"],
+  subsets: ["latin"],
+});
+
+export default function Home() {
+  return (
+    <main className="relative bg-[#13131391]">
+      <HeaderAnimation />
+
+      <div className="max-w-[500px] mx-auto pb-20 px-4">
+        <div className={`${plusJakartaSans.className} text-center mt-10`}>
+          <h1 className="text-[46px] leading-0 text-white mb-4">Agenda</h1>
+          <div className={`${chakra.className}`}>
+            <p className="text-white font-semibold leading-0 text-[25px] uppercase">
+              SE Orientation
+            </p>
+            <p className="text-white leading-0 text-[25px]">SESC - 2024/25</p>
+          </div>
+        </div>
+
+        <div
+          className={`${chakra.className} flex flex-row justify-center mt-6 mb-6`}>
+          <div className="inline-flex text-white items-center">
+            <CalendarFold size={36} />
+            <span className="ml-2 uppercase text-[19px] leading-none">
+              17th July 2024
+              <br />
+              1:30 PM
+            </span>
+          </div>
+
+          <div className="w-[0.5px] h-10 bg-white mx-[35px]"></div>
+
+          <div className="inline-flex text-white items-center">
+            <MapPin size={36} />
+            <span className="ml-2 uppercase text-[19px] leading-none">
+              Main
+              <br />
+              Auditorium
+            </span>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-y-4">
+          {AgendaData.map((event, index) => (
+            <EventCard
+              key={index}
+              title={event.title}
+              time={event.time}
+              imageURL={event.imageURL}
+              name={event.name}
+              position={event.position}
+              index={index}
+            />
+          ))}
+        </div>
+      </div>
+      <div className=" bg-white">
+        <div className="max-w-[500px] py-5 mx-auto flex flex-row items-center justify-center px-[150px] ">
+          <Image
+            src={"/logo/fcsc-logo.svg"}
+            width={150}
+            height={150}
+            alt=""
+            className="mr-4"
+          />
+          <div className="w-[0.5px] h-10 bg-black"></div>
+          <Image src={"/logo/sesc-logo.svg"} width={150} height={150} alt="" />
+        </div>
+      </div>
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,42 @@
-import EventCard from "@/components/EventCard";
 import HeaderAnimation from "@/components/HeaderAnimation/HeaderAnimation";
-import { AgendaData } from "@/data/AgendaData";
-import { CalendarFold, MapPin } from "lucide-react";
-import { Chakra_Petch, Plus_Jakarta_Sans } from "next/font/google";
+import LinkCard from "@/components/Links/LinkCard";
+import {
+  Calendar,
+  CalendarFold,
+  Facebook,
+  Instagram,
+  Linkedin,
+  MapPin,
+  QuoteIcon,
+  Shirt,
+  Vote,
+} from "lucide-react";
+import { Metadata } from "next";
+import { Plus_Jakarta_Sans } from "next/font/google";
 import Image from "next/image";
-import Link from "next/link";
 
 const plusJakartaSans = Plus_Jakarta_Sans({
   weight: ["400"],
   subsets: ["latin"],
 });
 
-const chakra = Chakra_Petch({
-  weight: ["400", "600"],
-  subsets: ["latin"],
-});
+export const metadata: Metadata = {
+  title: "Links | SESC SLIIT @ SE Orientation 2024 July",
+  description: "All agenda related to SESC SLIIT",
+};
 
-export default function Home() {
+export default function LinksPage() {
   return (
-    <main className="relative bg-[#13131391]">
+    <main className="relative min-h-screen bg-[#131313ba]">
       <HeaderAnimation />
 
-      <div className="max-w-[500px] mx-auto pb-20 px-4">
-        <div className={`${plusJakartaSans.className} text-center mt-10`}>
-          <h1 className="text-[46px] leading-0 text-white mb-4">Agenda</h1>
-          <div className={`${chakra.className}`}>
+      <div className="max-w-[500px] mx-auto pb-14 px-4">
+        <div className={`text-center mt-10`}>
+          <h1
+            className={`${plusJakartaSans.className} text-[46px] leading-0 text-white mb-4`}>
+            Links
+          </h1>
+          <div>
             <p className="text-white font-semibold leading-0 text-[25px] uppercase">
               SE Orientation
             </p>
@@ -32,54 +44,71 @@ export default function Home() {
           </div>
         </div>
 
-        <div
-          className={`${chakra.className} flex flex-row justify-center mt-6 mb-6`}>
-          <div className="inline-flex text-white items-center">
-            <CalendarFold size={36} />
-            <span className="ml-2 uppercase text-[19px] leading-none">
-              17th July 2024
-              <br />
-              1:30 PM
-            </span>
-          </div>
-
-          <div className="w-[0.5px] h-10 bg-white mx-[35px]"></div>
-
-          <div className="inline-flex text-white items-center">
-            <MapPin size={36} />
-            <span className="ml-2 uppercase text-[19px] leading-none">
-              Main
-              <br />
-              Auditorium
-            </span>
-          </div>
+        <div className="mt-6 mb-6 flex flex-col text-lg text-center text-white">
+          <QuoteIcon
+            size={24}
+            className="rotate-180 -ml-1 green-glow"
+            fill="#dbfd51"
+            stroke="none"
+          />
+          <p className="text-left">
+            If you have knowledge, let others light their candles at it.
+          </p>
+          <span className="self-end italic">—Margaret Fuller</span>
+          <QuoteIcon
+            size={18}
+            className="self-end green-glow"
+            fill="#dbfd51"
+            stroke="none"
+          />
         </div>
 
         <div className="flex flex-col gap-y-4">
-          {AgendaData.map((event, index) => (
-            <EventCard
-              key={index}
-              title={event.title}
-              time={event.time}
-              imageURL={event.imageURL}
-              name={event.name}
-              position={event.position}
-              index={index}
-            />
-          ))}
+          <div className="italic text-white">Featured</div>
+          <LinkCard
+            title="Event Agenda"
+            icon={<Calendar size={28} />}
+            url={"/agenda"}
+            featured
+          />
+          {/* <LinkCard
+            title="Vote for SE T-shirt"
+            icon={<Vote size={28} />}
+            url={"https://forms.gle/HTT1hJAQ8WjoQyv9A"}
+            featured
+          /> */}
+          <LinkCard
+            title="Hacknest"
+            icon={<Instagram size={28} />}
+            url={"https://instagram.com/hacknest.sesc"}
+            featured
+          />
+          <div className="pt-4 italic text-white">Social Links</div>
+          <LinkCard
+            title="Instagram"
+            icon={<Instagram size={28} />}
+            url={"https://www.instagram.com/sliit.sesc"}
+          />
+          <LinkCard
+            title="Facebook"
+            icon={<Facebook size={28} />}
+            url={"https://www.facebook.com/sliitsecommunity"}
+          />
+          <LinkCard
+            title="LinkedIn"
+            icon={<Linkedin size={28} />}
+            url={"https://www.linkedin.com/company/sesc-sliit"}
+          />
+          {/* <div className="pt-4 italic text-white">Other</div> */}
         </div>
       </div>
-      <div className=" bg-white">
-        <div className="max-w-[500px] py-5 mx-auto flex flex-row items-center justify-center px-[150px] ">
-          <Image
-            src={"/logo/fcsc-logo.svg"}
-            width={150}
-            height={150}
-            alt=""
-            className="mr-4"
-          />
-          <div className="w-[0.5px] h-10 bg-black"></div>
-          <Image src={"/logo/sesc-logo.svg"} width={150} height={150} alt="" />
+      <div className="">
+        <div className="max-w-[500px] text-white pb-12 px-5 text-center mx-auto flex flex-row items-center justify-center">
+          <p>
+            Software Engineering Student Community of SLIIT
+            <br />
+            July, 2024
+          </p>
         </div>
       </div>
     </main>
