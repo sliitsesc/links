88ad90bbbb1cb9cb118eea731182831ea3410aa5
pawan-live <pawan.senpura@gmail.com>
2025-07-22 21:12:06 +0530

    feat: add redirect for SE Day 25 form

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/se-day-25",
+        destination:
+          "https://docs.google.com/forms/d/e/1FAIpQLSfOM8A1A819VNWHNjj8RU0b7Olgu7tjPqwqYsSSB5phca8UGQ/viewform",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
